62be7f39a7a4900db73f3bcc67034f75c29c0e17
Julia Reh <juliareh@gmx.de>
2024-10-26 22:18:23 +0200

    first-components

---
+++ hachathon-phoenix-contact/src/app/main-page/main-page.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { LineChartComponent } from '../line-chart/line-chart.component';
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [LineChartComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+
+export class MainPageComponent {
+
+  
+  data: any;
+
+  options: any;
+
+  ngOnInit() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.data = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+              {
+                  label: 'First Dataset',
+                  data: [65, 59, 80, 81, 56, 55, 40],
+                  fill: false,
+                  borderColor: documentStyle.getPropertyValue('--blue-500'),
+                  tension: 0.4
+              },
+              {
+                  label: 'Second Dataset',
+                  data: [28, 48, 40, 19, 86, 27, 90],
+                  fill: false,
+                  borderColor: documentStyle.getPropertyValue('--pink-500'),
+                  tension: 0.4
+              }
+          ]
+      };
+
+      this.options = {
+          maintainAspectRatio: false,
+          aspectRatio: 0.6,
+          plugins: {
+              legend: {
+                  labels: {
+                      color: textColor
+                  }
+              }
+          },
+          scales: {
+              x: {
+                  ticks: {
+                      color: textColorSecondary
+                  },
+                  grid: {
+                      color: surfaceBorder,
+                      drawBorder: false
+                  }
+              },
+              y: {
+                  ticks: {
+                      color: textColorSecondary
+                  },
+                  grid: {
+                      color: surfaceBorder,
+                      drawBorder: false
+                  }
+              }
+          }
+      };
+  }
+
+
+}
